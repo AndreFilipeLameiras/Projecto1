842e50f8844f6bd679002adc277ea5c5b19b5ad5
Lameiras <afplameiras@gmail.com>
2021-10-13 11:14:02 +0100

    Criar Registos

---
+++ Projecto1/Models/GuestResponse.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projecto1.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
